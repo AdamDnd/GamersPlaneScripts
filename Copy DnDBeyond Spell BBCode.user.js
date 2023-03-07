1cd0b415283729091e3a483aac61a67cdc412697
AdamDnd <69721786+AdamDnd@users.noreply.github.com>
2023-03-07 07:28:14 +0000

    Copy all spell BBCode

--- Copy DnDBeyond Spell BBCode.user.js
+++ Copy DnDBeyond Spell BBCode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy DnDBeyond Spell BBCode
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @updateURL    https://github.com/AdamDnd/GamersPlaneScripts/raw/main/Copy%20DnDBeyond%20Spell%20BBCode.user.js
 // @downloadURL  https://github.com/AdamDnd/GamersPlaneScripts/raw/main/Copy%20DnDBeyond%20Spell%20BBCode.user.js
 // @description  Copies DnDBeyond to the clipboard for pasting into a character sheet
@@ -16,8 +16,10 @@
     'use strict';
 
     $('.info').each(function(){
-        $('<div style="text-align:right;cursor:pointer;color:#888;" class="copyGpBBCode"><span>'+$('.name .link',this).text()+' BBCode 📋<div>').insertAfter($(this));
+        $('<div style="text-align:right;cursor:pointer;color:#888;" class="copyGpBBCode">'+$('.name .link',this).text()+' BBCode 📋</div>').insertAfter($(this));
     });
+
+    $('<div style="text-align:right;cursor:pointer;color:#888;" class="copyGpAllBBCode">All BBCode 📋</div>').insertBefore($('.adohand-spells'));
 
     var notify=function(text){
           $("<div style='position: fixed;top: 0;bottom: 0;left: 0;right: 0;background-color: rgba(255,255,255,0.9);z-index: 60000;text-align: center;padding-top: 300px;font-size: 300%;font-weight: bold;'>"+text+"<br/><small style='font-size:50%;'>Paste into your Gamers' Plane character sheet</small></div>")
@@ -29,8 +31,8 @@
           });
     };
 
-    $('.listing').on('click','.copyGpBBCode',function(ev){
-        var infoBlock=$(this).prevAll('.info:first');
+    var getBBCode=function(copyGpBBCode,onComplete){
+        var infoBlock=copyGpBBCode.prevAll('.info:first');
         var miType=infoBlock.data('type');
         var miSlug=infoBlock.data('slug');
         $.get( "/"+miType+"/"+miSlug+"/more-info", function( data ) {
@@ -50,10 +52,45 @@
             text+='[/table]\r\n';
             text+=$.trim($('.more-info-body-description',dataBlock).text());
             text=text.replace(/[ ]+/g, ' ');
+            onComplete(text);
+        });
+    };
+
+    var getNextBBCode=function(copyGpBBCode,bbcode){
+        var infoBlock=copyGpBBCode.prevAll('.info:first');
+        $('#copyNotification').text($('.name .link',infoBlock).text());
+        getBBCode(copyGpBBCode,function(text){
+            if(bbcode.length>0){
+                bbcode+='\r\n\r\n';
+            }
+
+            bbcode+=text;
+            var nextcopyGpBBCode=copyGpBBCode.nextAll('.copyGpBBCode:first');
+            if(nextcopyGpBBCode.length==0){
+                navigator.clipboard.writeText(bbcode).then(function() {
+                    $('#copyNotification').fadeTo(1000,1.0, function(){
+                        $(this).fadeOut(1000, function(){
+                            $(this).remove();
+                        });
+                    });
+                });
+            } else {
+                getNextBBCode(nextcopyGpBBCode,bbcode);
+            }
+        });
+    };
+
+    $('.listing').on('click','.copyGpBBCode',function(ev){
+        getBBCode($(this),function(text){
             navigator.clipboard.writeText(text).then(function() {
                 notify('Copied');
             });
         });
+    });
+
+    $('.listing-container').on('click','.copyGpAllBBCode',function(ev){
+        $("<div style='position: fixed;top: 0;bottom: 0;left: 0;right: 0;background-color: rgba(255,255,255,0.9);z-index: 60000;text-align: center;padding-top: 300px;font-size: 300%;font-weight: bold;' id='copyNotification'>Copying...</div>").appendTo('body');
+        getNextBBCode($('.copyGpBBCode:first'),'');
     });
 
 })();
